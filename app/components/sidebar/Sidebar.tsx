7380a738ddba9f0ec0a7cf551db2d889f1663bb5
talha jubaer <talhajubaer018@gmail.com>
2024-03-20 23:18:31 +0600

    sidebars and mobile footer added

---
+++ app/components/sidebar/Sidebar.tsx
@@ -0,0 +1,22 @@
+// import getCurrentUser from '@/app/actions/getCurrentUser';
+
+import DesktopSidebar from "@/app/components/sidebar/DesktopSidebar";
+import MobileFooter from "@/app/components/sidebar/MobileFooter";
+
+async function Sidebar({ children }: {
+    children: React.ReactNode,
+}) {
+    // const currentUser = await getCurrentUser();
+
+    return (
+        <div className="h-full">
+            <DesktopSidebar />
+            <MobileFooter />
+            <main className="lg:pl-20 h-full">
+                {children}
+            </main>
+        </div>
+    )
+}
+
+export default Sidebar;
---
+++ app/users/layout.tsx
@@ -0,0 +1,16 @@
+// import getUsers from "../actions/getUsers";
+
+import Sidebar from "@/app/components/sidebar/Sidebar";
+
+export default async function UsersLayout({ children }: { children: React.ReactNode }) {
+    // const users = await getUsers();
+
+    return (
+        <Sidebar>
+            <div className="h-full">
+                {/* <UserList items={users} /> */}
+                {children}
+            </div>
+        </Sidebar>
+    );
+}
